6e0e48eefe73ba67f2b0de1fa9d63e625d1ecb8d
Diyar Faraj <diyar.faraj@gmail.com>
2023-07-01 14:00:19 +0200

    refactor

--- resources/ask_question.py
+++ resources/ask_question.py
@@ -32,26 +32,6 @@ pinecone.init(
 # Initialize OpenAI
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# _template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
-# You can assume the question about the most recent state of the union address.
-# Chat History:
-# {chat_history}
-# Follow Up Input: {question}
-# Standalone question:"""
-# CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
-
-# template = """You are an AI assistant for answering questions about the most recent state of the union address.
-# You are given the following extracted parts of a long document and a question. Provide a conversational answer.
-# If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
-# If the question is not about the most recent state of the union, politely inform them that you are tuned to only answer questions about the most recent state of the union.
-# end each answer with a a smile ":)"
-# Question: {question}
-# =========
-# {context}
-# =========
-# Answer in Markdown:"""
-# QA_PROMPT = PromptTemplate(template=template, input_variables=["question", "context"])
-
 
 class AskQuestion(Resource):
     def get(self):
