9857f724a7407b23d80c427005aeda324eedd710
Diyar Faraj <diyar.faraj@gmail.com>
2023-07-25 14:31:15 +0200

    refactore

--- utils/cosmos_client.py
+++ utils/cosmos_client.py
@@ -9,7 +9,6 @@ def create_cosmos_client():
     COSMOS_DB_ACCOUNT_URI = os.environ["COSMOS_DB_ACCOUNT_URI"]
     COSMOS_DB_ACCOUNT_KEY = os.environ["COSMOS_DB_ACCOUNT_KEY"]
     DATABASE_ID = os.environ["DATABASE_ID"]
-    CONTAINER_ID = os.environ["CONTAINER_ID"]
 
     client = CosmosClient(COSMOS_DB_ACCOUNT_URI, COSMOS_DB_ACCOUNT_KEY)
     database = client.get_database_client(DATABASE_ID)
